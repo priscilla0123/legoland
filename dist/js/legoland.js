f42e380510d715623f9649300451b3e191a34e93
sunjia032 <sunjia032@pingan.com.cn>
2017-01-06 18:00:14 +0800

    'update'

--- dist/js/legoland.js
+++ dist/js/legoland.js
@@ -1,20 +1,22 @@
-window.onload=function(){
+window.onload = function() {
     contentResize();
-} 
+}
 
-window.onresize=function(){
+window.onresize = function() {
     contentResize();
 }
 
 
-function contentResize(){
-    var bodyH=document.getElementsByClassName('lg-s-body-full')[0].offsetHeight;
-    var winH=document.body.clientHeight;
-    var navH=document.getElementsByClassName('lg-m-nav')[0].offsetHeight;
-    var breadH=document.getElementsByClassName('lg-m-breadcrumb')[0].offsetHeight;
-    var minH=winH-navH-breadH-30; 
-    if(bodyH<minH){  
-        document.getElementsByClassName('lg-s-body-full')[0].style.height=minH+'px';
-        console.log(bodyH,winH,navH,minH,document.getElementsByClassName('lg-s-body-full')[0].offsetHeight);
+function contentResize() {
+    if (document.getElementsByClassName('lg-s-body-full')[0]) {
+        var bodyH = document.getElementsByClassName('lg-s-body-full')[0].offsetHeight;
+        var winH = document.body.clientHeight;
+        var navH = document.getElementsByClassName('lg-m-nav')[0].offsetHeight;
+        var breadH = document.getElementsByClassName('lg-m-breadcrumb')[0].offsetHeight;
+        var minH = winH - navH - breadH - 30;
+        if (bodyH < minH) {
+            document.getElementsByClassName('lg-s-body-full')[0].style.height = minH + 'px';
+            console.log(bodyH, winH, navH, minH, document.getElementsByClassName('lg-s-body-full')[0].offsetHeight);
+        }
     } 
 }
