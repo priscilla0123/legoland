533e758e7a7fcd6d553883b716a53037452f3c4c
sunjia032 <sunjia032@pingan.com.cn>
2017-02-14 15:39:35 +0800

    updat

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,74 +1,27 @@
-function getJsFiles(grunt) {
-    var requires = [];
-    var path = require('path');
-    var jsFiles = grunt.file.expand('src/**/*.js');
-    var pass = {};
-
-    function getDeps(file) {
-        var rs = [];
-
-        if (pass[file]) {
-            return [];
-        }
-
-        pass[file] = true;
-
-        var reg = /require\(['"]([^'"]+)['"]\)/g;
-        var content = grunt.file.read(file),
-            result, rs = [];
-
-        while (result = reg.exec(content)) {
-            if (result[1]) {
-                var dep = path.join(path.dirname(file), result[1]).replace(/\\+/g, '/') + '.js';
-
-                if (!grunt.file.exists(dep)) {
-                    continue;
-                }
-
-                rs = rs.concat(getDeps(dep));
-                rs.push(dep);
-            }
-        }
-
-        rs.push(file);
-
-        return rs;
-    }
-
-    jsFiles.map(function(file) {
-        var deps = getDeps(file);
-        requires = requires.concat(deps);
-    });
-
-    var exists = {};
-
-    return requires.concat(jsFiles).filter(function(file) {
-        if (!exists[file]) {
-            exists[file] = true;
-            return true;
-        }
-
-        return false;
-    });
-}
-
 module.exports = function(grunt) {
-    var jsFiles = getJsFiles(grunt),
-        cssFiles = [];
-    //console.log(jsFiles);
-    jsFiles.forEach(function(file) {
-        file = file.replace('.js', '.css');
-
-        if (grunt.file.exists(file)) {
-            cssFiles.push(file);
-        }
-    });
-
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
         copy: {
-            dev: {
+            debug: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/css/icon',
+                    src: 'iconfont.ttf',
+                    dest: 'test'
+                }, {
+                    expand: true,
+                    cwd: 'src/css/icon',
+                    src: 'iconfont.woff',
+                    dest: 'test'
+                }, {
+                    expand: true,
+                    cwd: 'examples',
+                    src: '*.*',
+                    dest: 'test/examples'
+                }]
+            },
+            release: {
                 files: [{
                     expand: true,
                     cwd: 'src/css/icon',
@@ -81,99 +34,51 @@ module.exports = function(grunt) {
                     dest: 'dist'
                 }, {
                     expand: true,
-                    cwd: 'src/js',
+                    cwd: 'examples',
                     src: '*.*',
-                    dest: 'dist/js'
-                }, {
-                    expand: true,
-                    cwd: 'demo',
-                    src: '*.*',
-                    dest: 'dist/demo'
-                }]
-            },
-            release: {
-
-                files: [{
-                    expand: true,
-                    cwd: 'dist',
-                    src: '*/*.*',
-                    dest: 'release'
-                },{
-                    expand: true,
-                    cwd: 'dist',
-                    src: '*.*',
-                    dest: 'release'
+                    dest: 'dist/examples'
                 }]
             },
         },
-  
-        concat: {
-            js: {
-                options: {
-                    separator: ';',
-                    process: function(content) {
-                        return content.replace(/if\s*\(\s*typeof\s+define[\s\S]+?\}\s*else\s*\{([\s\S]*?)\}\s*(?=\}\)|$)/, function(all, $1) {
-                            return '\t' + $1.trim() + '\n';
-                        });
-                    }
-                },
 
-                files: {
-                    'dist/<%=pkg.name%>.js': jsFiles
-                }
-            },
-
-            css: {
-                options: {
-                    process: function(content, file) {
-                        return content.replace(/\.\//g, function(all) {
-                            return all + file.split('/')[1] + '/';
-                        });
-                    }
-                },
-
-                files: {
-                    'dist/<%=pkg.name%>.css': cssFiles
-                }
-            }
-        },
-
-        uglify: {
-            options: {
-                sourceMap: true,
-                sourceMapName: 'dist/<%=pkg.name%>-min.js.map'
-            },
-            build: {
-                src: 'dist/<%=pkg.name%>.js',
-                dest: 'dist/<%=pkg.name%>-min.js'
-            }
-        },
-
-        //压缩CSS
-        cssmin: {
-            build: {
-                files: {
-                    'dist/<%=pkg.name%>-min.css': 'dist/<%=pkg.name%>.css'
-                }
-            }
-        },
         less: {
-            build: {
+            debug: {
                 options: {
-                    //paths: ["assets/css"], // @import 加载文件的路径
-                    cleancss: true // 压缩css文件 
+                    cleancss: false // 压缩css文件 
                 },
                 files: {
-                    "dist/legoland.min.css": "src/legoland.css",
-                    "../new/doc-backend/doc/legoland/legoland.min.css": "src/legoland.css"
+                    "test/legoland.css": "src/legoland.css"
+                }
+            },
+
+            release: {
+                options: {
+                    cleancss: false // 压缩css文件 
+                },
+                files: {
+                    "dist/legoland.css": "src/legoland.css"
                 }
             }
         },
+
+        cssmin: {
+            release: {
+                files: {
+                    "dist/legoland.min.css": "dist/legoland.css"
+                }
+            }
+        },
+
         watch: {
             start: {
-                files: ['src/css/*/*.css', 'src/js/*.js', 'demo/*.*'],
+                files: ['src/css/*/*.css', 'src/js/*.js', 'examples/*.*'],
                 tasks: ['less', 'copy']
             }
+        },
+
+        clean: {
+            debug: ['./dist'],
+            release: ['./test', './dist']
         }
     });
 
@@ -181,11 +86,10 @@ module.exports = function(grunt) {
         grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
     });
     grunt.loadNpmTasks('grunt-contrib-copy');
-    // grunt.loadNpmTasks('grunt-contrib-concat');
-    // grunt.loadNpmTasks('grunt-contrib-uglify');
-    // grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('default', ['less', 'copy:dev', 'watch']);
-    grunt.registerTask('rel', ['less', 'copy:dev','copy:release']);
+    grunt.registerTask('default', ['clean:debug', 'copy:debug', 'less:debug', 'watch']);
+    grunt.registerTask('release', ['clean:release', 'copy:release', 'less:release', 'cssmin:release']);
 };
